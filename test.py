6a27e79b68d61c465b60e87d75915e7fac108026
Veronika Schwan <github@pisquaredover6.de>
2020-02-17 22:20:17 +0100

    add test for make_filename

--- test.py
+++ test.py
@@ -1,20 +1,50 @@
 import os
 import unittest
 from image_crawler import crawl_images
+from image_crawler import make_filename
 
 
 class MyTestCase(unittest.TestCase):
     input_file = 'testdata/urllist.txt'
     output_directory = 'test_output'
 
+    def setUp(self):
+        try:
+            os.mkdir(self.output_directory)
+        except FileExistsError:
+            pass
+        os.system('rm -rf {}/*'.format(self.output_directory))
+
+    def tearDown(self):
+        os.system('rm -rf {}'.format(self.output_directory))
+
     def test_load_images(self):
         crawl_images(input_file=self.input_file, output_directory=self.output_directory)
         num_of_images = len(os.listdir(self.output_directory))
         self.assertEqual(
-            28,
+            3,
             num_of_images,
             'Number of received images should be {}, was {}!'.format(28, num_of_images)
         )
+
+    def test_make_filename(self):
+        url = 'http://mywebserver.com/images/24174'
+        file_extension = 'jpeg'
+        filename = make_filename(self.output_directory, file_extension, url)
+        filename_solution = '{}/24174.jpeg'.format(self.output_directory)
+        self.assertEqual(filename_solution, filename, 'Image file name is wrong! Check extension')
+
+        url = 'http://mywebserver.com/images/271947.png'
+        file_extension = 'jpeg'
+        filename = make_filename(self.output_directory, file_extension, url)
+        filename_solution = '{}/271947.jpeg'.format(self.output_directory)
+        self.assertEqual(filename_solution, filename, 'Image file name is wrong! Check extension')
+
+        url = 'http://mywebserver.com/images/992147.png'
+        file_extension = 'png'
+        filename = make_filename(self.output_directory, file_extension, url)
+        filename_solution = '{}/992147.png'.format(self.output_directory)
+        self.assertEqual(filename_solution, filename, 'Image file name is wrong!')
 
 
 if __name__ == '__main__':
