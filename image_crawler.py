6d9ebf9b56791bc08ce942c801134ec8d8332757
Veronika Schwan <github@pisquaredover6.de>
2020-02-17 21:53:42 +0100

    script to function, added unique filename generation

--- image_crawler.py
+++ image_crawler.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
+import imghdr
 import logging
 import requests
 import sys
+from os import path
 
 logging.basicConfig(
     format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
@@ -13,18 +14,55 @@ logging.basicConfig(
 logger = logging.getLogger('')
 
 
-with open(sys.argv[1], 'r') as input_list:
-    output_directory = sys.argv[2]
-    # read plaintext file line by line
-    for line in input_list:
-        url = line.rstrip('\n')
-        try:
-            image_request = requests.get(url)
-        except Exception as e:
-            logger.error('Error at image {} - ignoring. {}'.format(url, e))
-            continue
+def crawl_images(input_file, output_directory='output'):
+    """
+    read a plaintext file, fetch the image and store it on disk
+    In case of error, the image url is ignored and the script continues
+    :param input_file: plaintext file containing one image url per line
+    :param output_directory: target directory to save the collected images
+    :return: nothing
+    """
+    line_counter = 0
+    saved_images_counter = 0
+    with open(input_file, 'r') as input_list:
+        # read plaintext file line by line
+        for line in input_list:
+            url = line.rstrip('\n')
+            try:
+                image_request = requests.get(url)
+            except Exception as e:
+                logger.error('Error at image {} - ignoring. {}'.format(url, e))
+                line_counter += 1
+                continue
 
-        if image_request.status_code == 200:
-            filename = '{}/{}'.format(output_directory, url.rsplit('/', 1)[1])
-            with open(filename, 'wb') as image_file:
-                image_file.write(image_request.content)
+            if image_request.status_code == 200:
+                # find out type of image (png, jpg...)
+                file_extension = imghdr.what("dummy", h=image_request.content)
+                if not file_extension:
+                    logger.error('Not an image file - ignoring: {}'.format(url))
+                else:
+                    filename = '{}/{}'.format(output_directory, url.rsplit('/', 1)[1])
+                    try:
+                        filepath, extension = filename.rsplit('.')
+                        assert extension == file_extension
+                    except ValueError:
+                        filename = '{}.{}'.format(filename, file_extension)
+                    except AssertionError:
+                        filename = '{}.{}'.format(filepath, file_extension)
+                    filename_counter = 1
+                    # if filename is already taken, attatch a number (counting up) until we find a free name
+                    while path.exists(filename):
+                        filepath, extension = filename.rsplit('.')
+                        filename = '{}_({}).{}'.format(filepath, filename_counter, extension)
+                        filename_counter += 1
+                    with open(filename, 'wb') as image_file:
+                        image_file.write(image_request.content)
+                    saved_images_counter += 1
+            else:
+                logger.error('Error retrieving image {}: Error {}'.format(url, image_request.status_code))
+            line_counter += 1
+        logger.info('Processed {} lines, saved {} images'.format(line_counter, saved_images_counter,))
+
+
+if __name__ == '__main__':
+    crawl_images(input_file=sys.argv[1])
